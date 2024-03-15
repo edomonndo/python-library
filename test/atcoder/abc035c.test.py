231233c966fe9acea50d32610843459fa4d5e0d2
edomonndo <osqnq1@yahoo.co.jp>
2024-03-16 00:48:27 +0900

    add example of lazy segtree

---
+++ test/atcoder/abc035c.test.py
@@ -0,0 +1,22 @@
+# verification-helper: IGNORE https://atcoder.jp/contests/abc035/tasks/abc035_c
+from atcoder.lazysegtree import LazySegTree
+
+
+def mapping(f, x):
+    return not x if f else x
+
+
+def composition(g, f):
+    return g ^ f
+
+
+ID = 0
+
+n, q = map(int, input().split())
+st = LazySegTree(max, -1, mapping, composition, ID, [0] * n)
+
+for _ in range(q):
+    l, r = map(int, input().split())
+    st.apply(l - 1, r, 1)
+
+print("".join("1" if st.get(i) else "0" for i in range(n)))
