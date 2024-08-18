69e29bcde7aae1b6430886304723906bfd46313d
edomonndo <osqnq1@yahoo.co.jp>
2024-08-18 17:02:41 +0900

    update

--- graph/scc_incremental.py
+++ graph/scc_incremental.py
@@ -1,41 +1,45 @@
 from graph.scc import scc
 
 
-def incremental_scc(n: int, edges: list[tuple[int, int]]):
+def incremental_scc(n: int, edges: list[tuple[int, int]]) -> list[int]:
     m = len(edges)
     inf = float("inf")
     merge_time = [inf] * m
-    dat = [(i, u, v) for i, (u, v) in enumerate(edges)]
+    dat = [(ei, u, v) for ei, (u, v) in enumerate(edges)]
 
     new_idx = [-1] * n
-    st = [(0, m + 1, dat)]
+    cnt = 1
+    st = [(0, m, dat)]
     while st:
         l, r, dat = st.pop()
-        mid = (l + r) >> 1
-        n_ = 0
-        for _, u, v in dat:
-            if new_idx[u] == -1:
-                new_idx[u] = n_
-                n_ += 1
-            if new_idx[v] == -1:
-                new_idx[v] = n_
-                n_ += 1
-        es = [(new_idx[u], new_idx[v]) for i, u, v in dat if i < mid]
-        _, comp = scc(n_, es)
-        dat1, dat2 = [], []
-        for i, u, v in dat:
-            u, v = new_idx[u], new_idx[v]
-            if i < mid:
+        mid = (l + r + 1) >> 1
+        start = cnt
+        for i in range(len(dat)):
+            ei, u, v = dat[i]
+            if new_idx[u] < start:
+                new_idx[u] = cnt
+                cnt += 1
+            if new_idx[v] < start:
+                new_idx[v] = cnt
+                cnt += 1
+            dat[i] = (ei, new_idx[u] - start, new_idx[v] - start)
+
+        _, comp = scc(cnt - start, [(u, v) for ei, u, v in dat if ei < mid])
+        if l + 1 == r:
+            for ei, u, v in dat:
                 if comp[u] == comp[v]:
-                    if merge_time[i] > mid:
-                        merge_time[i] = mid
-                        dat1.append((i, u, v))
-                else:
-                    dat2.append((i, comp[u], comp[v]))
+                    merge_time[ei] = r
+            continue
+        j = 0
+        k = len(dat)
+        for _ in range(len(dat)):
+            ei, u, v = dat[j]
+            if ei < mid and comp[u] == comp[v]:
+                j += 1
             else:
-                dat2.append((i, comp[u], comp[v]))
-        if dat2 and r - mid > 1:
-            st.append((mid, r, dat2))
-        if dat1 and mid - l > 1:
-            st.append((l, mid, dat1))
+                dat[j] = (ei, comp[u], comp[v])
+                k -= 1
+                dat[j], dat[k] = dat[k], dat[j]
+        st.append((mid, r, dat[j:]))
+        st.append((l, mid, dat[:j]))
     return merge_time
--- test/library_checker/graph/scc_incremental.test.py
+++ test/library_checker/graph/scc_incremental.test.py
@@ -1,5 +1,7 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/incremental_scc
 
+from collections import defaultdict
+
 from graph.scc_incremental import incremental_scc
 from graph.connectivity.unionfind import UnionFind
 
@@ -9,21 +11,23 @@ n, m = map(int, input().split())
 X = [int(x) for x in input().split()]
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 time = incremental_scc(n, edges)
-ids = [[] for _ in range(m + 1)]
+ids = defaultdict(list)
 for ei in range(m):
     if time[ei] <= m:
         ids[time[ei]].append(ei)
 
 uf = UnionFind(n)
-ans = 0
-for t in range(1, m + 1):
+ans = [0] * m
+for t in sorted(ids.keys()):
     for ei in ids[t]:
         u, v = edges[ei]
         u, v = uf.leader(u), uf.leader(v)
         if u == v:
             continue
-        ans += X[u] * X[v] % MOD
-        ans %= MOD
+        ans[t - 1] += X[u] * X[v] % MOD
+        ans[t - 1] %= MOD
         uf.merge(u, v)
         X[uf.leader(u)] = (X[u] + X[v]) % MOD
-    print(ans)
+for i in range(m - 1):
+    ans[i + 1] = (ans[i + 1] + ans[i]) % MOD
+print(*ans, sep="\n")
