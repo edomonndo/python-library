6f28b81b89f2d4f59137fa7f49b2e889503c305f
edomonndo <osqnq1@yahoo.co.jp>
2024-09-14 02:16:15 +0900

    update

--- test/aoj/grl/grl_3_c_strongly_connected_components.test.py
+++ test/aoj/grl/grl_3_c_strongly_connected_components.test.py
@@ -1,13 +1,13 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_3_C
 
-from graph.scc import scc
+from graph.scc import SCC
 
 n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-groups, comp_num = scc(n, edges)
-
+scc = SCC(n, edges)
+cc = scc.get_mapping()
 q = int(input())
 for _ in range(q):
     s, t = map(int, input().split())
-    print(1 if comp_num[s] == comp_num[t] else 0)
+    print(1 if cc[s] == cc[t] else 0)
